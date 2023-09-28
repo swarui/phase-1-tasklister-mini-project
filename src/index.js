69cf2a810fa64a9330d061ea8f6ad15d56a5a2e8
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-09-28 09:42:43 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#create-task-form");
+  const tasksList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = document.querySelector("#new-task-description").value;
+
+const listItem = document.createElement("li");
+listItem.textContent = inputValue;
+
+const removeButton = document.createElement("button");
+removeButton.textContent = "Remove Task";
+removeButton.addEventListener("click", (e) => {
+  listItem.remove();
+});
+
+listItem.appendChild(removeButton);
+tasksList.appendChild(listItem);
+form.reset();
+  });
+});
